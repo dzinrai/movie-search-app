78d4c79ae35a53af927589f2c60fb10b94bdd4f9
dzinrai <dzinrai@mail.ru>
2020-05-03 22:42:47 +0300

    fix: refactor getMovies

--- movie-search/src/getMovies.js
+++ movie-search/src/getMovies.js
@@ -5,15 +5,15 @@ export default async function getMovies(search, page = 1) {
     const url = `https://www.omdbapi.com/?s=${search}&page=${page}&apikey=4679477d`;
     const key = `${search}__page${page}`;
     let data = localStorage.getItem(key);
+    let result;
     if (data) {
         data = await JSON.parse(data);
-        const slides = await createSlides(data);
-        return slides;
+    } else {
+        result = await fetch(url);
+        data = await result.json();
+        localStorage.setItem(key, JSON.stringify(data));
     }
-    const result = await fetch(url);
-    const movies = await result.json();
-    localStorage.setItem(key, JSON.stringify(movies));
-    const slides = await createSlides(movies);
+    const slides = createSlides(data);
     return slides;
     // {"Response":"False","Error":"Movie not found!"}
 }
